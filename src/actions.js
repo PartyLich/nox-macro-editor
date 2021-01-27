3fe78e6bc2e57171880f60f29996575a2220a0b1
Arthur E. Jones <PartyLich@gmail.com>
2021-01-27 11:37:44 -0600

    feat: add core types

---
+++ src/actions.js
@@ -0,0 +1,83 @@
+// @flow
+
+// actions
+const CLICK: 'CLICK' = 'CLICK';
+const MRELEASE: 'MRELEASE' = 'MRELEASE';
+const MDRAG: 'MDRAG' = 'MDRAG';
+const WAIT: 'WAIT' = 'WAIT';
+const NONE: 'NONE' = 'NONE';
+
+export type Coord = {|
+  x: number,
+  y: number,
+|};
+
+type ActionA<Type, Payload> = {
+  type: Type,
+  ...Payload,
+};
+
+type Empty = {||};
+
+type Wait = {|
+  duration: number,
+|};
+export type WaitAction = ActionA<typeof WAIT, Wait>;
+
+export type ClickAction = ActionA<typeof CLICK, Coord>;
+export type DragAction = ActionA<typeof MDRAG, Coord>;
+
+export type NoneAction = ActionA<typeof NONE, Empty>;
+export type MReleaseAction = ActionA<typeof MRELEASE, Empty>;
+
+export type Action =
+  | ClickAction
+  | DragAction
+  | MReleaseAction
+  | NoneAction
+  | WaitAction
+  ;
+
+export type ActionType =
+  | typeof CLICK
+  | typeof MDRAG
+  | typeof MRELEASE
+  | typeof NONE
+  | typeof WAIT
+  ;
+
+// Wait action creator
+export const waitAction = (duration: number): WaitAction => ({
+  type: WAIT,
+  duration,
+});
+
+// Click action creator
+export const clickAction = (coord: Coord): ClickAction => ({
+  type: CLICK,
+  ...coord,
+});
+
+// None action creator
+export const noneAction = (): NoneAction => ({
+  type: NONE,
+});
+
+// Drag action creator
+export const dragAction = (coord: Coord): DragAction => ({
+  type: MDRAG,
+  ...coord,
+});
+
+// MRelease action creator
+export const releaseAction = (): MReleaseAction => ({
+  type: MRELEASE,
+});
+
+export const types = {
+  WAIT,
+  CLICK,
+  MRELEASE,
+  MDRAG,
+  NONE,
+};
