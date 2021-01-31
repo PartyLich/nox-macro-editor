5d66e80c56a046354c8adbbf13b903d7bebc4aff
Arthur E. Jones <PartyLich@gmail.com>
2021-01-31 17:57:39 -0600

    feat: add IntegerInput component

--- components/index.js
+++ components/index.js
@@ -1,4 +1,5 @@
 export { default as ActionItem } from './ActionItem';
 export { default as ActionList } from './ActionList';
 export { default as Controls } from './Controls';
+export { default as IntegerInput } from './IntegerInput';
 export { default as Editor } from './Editor';
