4f76e33f466687f2a0a6697c5c997b3251c81d15
Arthur E. Jones <PartyLich@gmail.com>
2021-01-31 16:21:26 -0600

    feat: load and deserialize nox macros

--- components/Editor.js
+++ components/Editor.js
@@ -1,15 +1,38 @@
 // @flow
-import React from 'react';
+import React, { useState } from 'react';
 
+import { pipe, trace } from '../src/util';
+import { deserialize } from '../src/serialize';
 import { ActionList, Controls } from '.';
+import type { Action } from '../src/actions.js';
 
 import styles from './Editor.module.scss';
 
 
+const handleText: (function) => (string) => void = (setStateFn) => pipe(
+    trace('handleText'),
+    deserialize,
+    (result) => {
+      setStateFn(result);
+      return result;
+    },
+);
+
+const onFileSelect = (setStateFn) => (evt) => {
+  const fileList = evt.target.files;
+
+  fileList.item(0)
+      .text()
+      .then(handleText(setStateFn));
+};
+
+
 const Editor = () => {
+  const [actions: Array<Action>, setActions] = useState([]);
+
   return (
     <>
-      <input type="file" />
+      <input type="file" onChange={onFileSelect(setActions)} />
       <div className={styles.container}>
         <ActionList />
         <Controls />
