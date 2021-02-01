93fcbd3491900aa2fcc6a1c63dc9055fc595d597
Arthur E. Jones <PartyLich@gmail.com>
2021-01-31 23:23:55 -0600

    feat: add download function

--- src/util.js
+++ src/util.js
@@ -31,7 +31,21 @@ const reorder = <T>(arr: Array<T>): ((number, number) => Array<T>) =>
 // return true if string contains an integer (base 10)
 const isInt: PredicateFn<string> = (str) => /^[+-]?\d+$/.test(str.trim());
 
+// initiate a file download with the specified `contentType` and `content`
+// all over the net. versions on blogs, SO, etc. No idea who the originator was
+const download = (contentType: string, content: any, filename: string) => {
+  const file = new Blob([content], { type: contentType });
+  const a = document.createElement('a');
+
+  a.href = URL.createObjectURL(file);
+  a.download = filename;
+  a.click();
+
+  URL.revokeObjectURL(a.href);
+};
+
 export {
+  download,
   filter,
   isInt,
   map,
