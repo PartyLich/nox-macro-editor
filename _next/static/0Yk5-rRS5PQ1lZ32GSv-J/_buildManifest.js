747066e4f3daf0da270037aa3a19a8ed4f2f944f
PartyLich <partylich@gmail.com>
2021-02-02 23:31:52 +0000

    Deploying to gh-pages from  @ 817bc09879632d907782b7eaf16457b5193dddd6 🚀

--- _next/static/0Yk5-rRS5PQ1lZ32GSv-J/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002F67c88f0189b31a35ccac.css","static\u002Fchunks\u002Fpages\u002Findex-6d74ec9e362f467d883d.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-9c0f9506139486bb6926.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
