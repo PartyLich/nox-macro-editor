e3c107b7680a92bc03177f0539e23bc2fbacd5e2
Arthur E. Jones <PartyLich@gmail.com>
2021-02-02 16:44:53 -0600

    feat: add removeAt array util

--- test/util.test.js
+++ test/util.test.js
@@ -4,6 +4,7 @@ import {
   reorder,
   insert,
   isInt,
+  removeAt,
 } from '../src/util';
 
 
@@ -78,6 +79,45 @@ test('insert()', (t) => {
     const data = [1];
     const actual = insert([2, 3], -1)( data);
     t.deepEqual(actual, expected, msg);
+  }
+
+  t.end();
+});
+
+test('removeAt()', (t) => {
+  {
+    const msg = 'removes element at index';
+    const expected = [1, 3];
+    const actual = removeAt(1, [1, 2, 3]);
+    t.deepEqual(actual, expected, msg);
+  }
+  {
+    const msg = 'returns array copy if index is out of bounds';
+    const expected = [1, 2, 3];
+    {
+      const actual = removeAt(42, [1, 2, 3]);
+      t.deepEqual(actual, expected, msg);
+    }
+    {
+      const actual = removeAt(-1, [1, 2, 3]);
+      t.deepEqual(actual, expected, msg);
+    }
+    {
+      // $FlowFixMe flow failed to catch a call like this
+      const actual = removeAt(undefined, [1, 2, 3]);
+      t.deepEqual(actual, expected, msg);
+    }
+    {
+      // $FlowFixMe flow failed to catch a call like this
+      const actual = removeAt(null, [1, 2, 3]);
+      t.deepEqual(actual, expected, msg);
+    }
+    {
+      const msg = 'original array copied';
+      const actual = removeAt(42, [1, 2, 3]);
+      actual[0] = 42;
+      t.notDeepEqual(actual, expected, msg);
+    }
   }
 
   t.end();
