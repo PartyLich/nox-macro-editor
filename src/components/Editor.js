b04b61f3dd8a5f9ed34d008c765a5cc41f084f33
Arthur E. Jones <PartyLich@gmail.com>
2021-02-05 14:49:10 -0600

    feat: scale macro to current resolution on import

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -12,7 +12,7 @@ import {
 import { deserialize, serialize } from '../serialize';
 import { ActionList, Controls } from '.';
 import { types } from '../actions';
-import type { Action } from '../actions';
+import type { Action, Coord } from '../actions';
 
 import styles from './Editor.module.scss';
 
@@ -70,19 +70,66 @@ const updateAction = (arr: Array<Action>): UpdateActionType =>
     return res;
   };
 
+// shallow object comparison. true if `b` contains all the keys of `a` with
+// matching values
+const shallowEqual = (a, b): boolean => Object.keys(a).reduce(
+    (acc, key) => a[key] === b[key], true,
+);
+
+const scale = (from: number, to: number, num: number): number => {
+  const factor = to / from;
+
+  return num * factor;
+};
+
+// scale an action from one resolution (`fromRes`) to another (`toRes`)
+const scaleAction = (fromRes: Coord, toRes: Coord) =>
+  (action: Action): Action => {
+    switch (action.type) {
+      case types.CLICK:
+        return {
+          ...action,
+          x: scale(fromRes.x, toRes.x, action.x),
+          y: scale(fromRes.y, toRes.y, action.y),
+        };
+      case types.MDRAG:
+        return {
+          ...action,
+          x: scale(fromRes.x, toRes.x, action.x),
+          y: scale(fromRes.y, toRes.y, action.y),
+        };
+    }
+
+    return action;
+  };
+
 // import a macro, inserting its Actions after the selected index
-const importFile = (setStateFn: function ) =>
-  (actions: Array<Action>, selected: ?number, fileText: string ) => () => {
-    const ind = (selected === null)
+const importFile = (setStateFn: function ) => (
+    actions: Array<Action>,
+    selected: ?number,
+    resolution: Coord,
+    fileText: string,
+) => () => {
+  const ind = (selected === null)
       ? actions.length
       : selected + 1;
 
-    pipe(
-        deserialize,
-        insert(actions, ind),
-        setStateFn,
-    )(fileText);
-  };
+  pipe(
+      deserialize,
+      (_actions) => {
+        const actions = _actions.map(([action, _]) => action);
+
+        const [, importedRes] = _actions[0];
+        if (!shallowEqual(resolution, importedRes)) {
+          return actions.map(scaleAction(importedRes, resolution));
+        }
+
+        return actions;
+      },
+      insert(actions, ind),
+      setStateFn,
+  )(fileText);
+};
 
 // load a macro, replacing all Actions with the file's content
 const loadFile = (setActions: function, setResolution: function) =>
@@ -120,7 +167,12 @@ const Editor = () => {
           <button onClick={loadFile(setActions, setResolution)(fileText)}
           >Load
           </button>
-          <button onClick={importFile(setActions)(actions, selected, fileText)}
+          <button onClick={importFile(setActions)(
+              actions,
+              selected,
+              resolution,
+              fileText,
+          )}
           >Import
           </button>
           <button onClick={saveFile}
