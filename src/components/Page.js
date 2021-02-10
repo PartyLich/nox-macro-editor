c836d254d66709fddc60b014b3984de7d0808738
Arthur E. Jones <PartyLich@gmail.com>
2021-02-09 18:54:30 -0600

    feat: add Page component

---
+++ src/components/Page.js
@@ -0,0 +1,29 @@
+// @flow
+import React, { type Node } from 'react';
+import Head from 'next/head';
+
+import { Layout } from '.';
+import styles from './Page.module.scss';
+
+
+type Props = {
+  children: Node,
+  title?: string,
+}
+
+const Page = ({ children, title = 'This is the default title' }: Props) => (
+  <section className={styles.body}>
+    <Head>
+      <title>{title}</title>
+      <meta
+        name="viewport"
+        content="minimum-scale=1, initial-scale=1, width=device-width"
+      />
+    </Head>
+    <Layout >
+      {children}
+    </Layout>
+  </section>
+);
+
+export default Page;
--- src/components/index.js
+++ src/components/index.js
@@ -7,4 +7,6 @@ export { default as Editor } from './Editor';
 export { default as Footer } from './Footer';
 export { default as Header } from './Header';
 export { default as Layout } from './Layout';
+export { default as Page } from './Page';
+
 export { default as RemovableItem } from './RemovableItem';
