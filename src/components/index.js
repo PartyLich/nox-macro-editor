d6d691f7f73856565fa35cfc6ad982b08d39d128
Arthur E. Jones <PartyLich@gmail.com>
2021-02-09 18:36:53 -0600

    feat: add Layout component

--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,8 @@ export { default as ActionList } from './ActionList';
 export { default as Controls } from './Controls';
 export { default as IntegerInput } from './IntegerInput';
 export { default as Editor } from './Editor';
+
 export { default as Footer } from './Footer';
 export { default as Header } from './Header';
+export { default as Layout } from './Layout';
 export { default as RemovableItem } from './RemovableItem';
