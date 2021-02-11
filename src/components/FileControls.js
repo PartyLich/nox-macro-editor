c654d24615dba71dd1fb6f3b42d066c8be02295f
Arthur E. Jones <PartyLich@gmail.com>
2021-02-10 22:27:11 -0600

    feat: show confirmation dialog before file load

--- src/components/FileControls.js
+++ src/components/FileControls.js
@@ -1,10 +1,13 @@
 // @flow
-import React from 'react';
+import React, { useState } from 'react';
 import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
 import AddToPhotosIcon from '@material-ui/icons/AddToPhotos';
 import InsertDriveFileIcon from '@material-ui/icons/InsertDriveFile';
 import SaveIcon from '@material-ui/icons/Save';
+
+import { ConfirmDialog } from '.';
+import { pipe } from '../util';
 
 import styles from './FileControls.module.scss';
 
@@ -22,6 +25,19 @@ const FileControls = ({
   handleImport,
   saveFile,
 }: Props) => {
+  const DESCRIPTION = `Loading the selected file will overwrite any macro currently in the editor.`;
+  const TITLE = 'Replace the current macro?';
+  const [isAlertOpen, setAlertOpen] = useState(false);
+
+  const closeAlert = () => setAlertOpen(false);
+
+  const openAlert = () => setAlertOpen(true);
+
+  const loadFile = pipe(
+      handleLoad,
+      closeAlert,
+  );
+
   return (
     <div className={[styles.container, styles.controls].join(' ')}>
       <div>
@@ -30,7 +46,7 @@ const FileControls = ({
       <div className={styles.container}>
         <Button
           color="primary"
-          onClick={handleLoad}
+          onClick={openAlert}
           variant="contained"
           size="small"
           startIcon={<InsertDriveFileIcon />}
@@ -57,6 +73,13 @@ const FileControls = ({
           </Button>
         </Box>
       </div>
+      <ConfirmDialog
+        title={TITLE}
+        description={DESCRIPTION}
+        handleYes={loadFile}
+        handleNo={closeAlert}
+        open={isAlertOpen}
+      />
     </div>
   );
 };
