10587c181179572b227a5a456cf3471721c53e8c
Arthur E. Jones <PartyLich@gmail.com>
2021-03-02 18:49:52 -0600

    refactor: update FileInput type annotations

--- src/components/FileInput.js
+++ src/components/FileInput.js
@@ -1,5 +1,5 @@
 // @flow
-import React from 'react';
+import React, { type Element } from 'react';
 import Button from '@material-ui/core/Button';
 import { useId } from 'react-id-generator';
 
@@ -10,9 +10,11 @@ type Props = {
   onChange: function,
 };
 
-const FileInput = ({
+type signature = (Props) => Element<"div">;
+
+const FileInput: signature = ({
   onChange,
-}: Props) => {
+}) => {
   const [id] = useId();
 
   return (
