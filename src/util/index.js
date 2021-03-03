25244ce353844d8301b28090d9fd41fb7d58268c
Arthur E. Jones <PartyLich@gmail.com>
2021-03-03 15:15:46 -0600

    docs: update documentation/comments

--- src/util/index.js
+++ src/util/index.js
@@ -2,14 +2,24 @@
 export type PredicateFn<T> = (T) => boolean;
 
 export { default as download } from './download';
-export { default as ensure } from './ensure';
+
+// Array
+export { default as reorder } from './reorder';
+export { default as removeAt } from './removeAt';
+
+// Logic
+export * from './compare';
+
+// Pointfree
 export { default as filter } from './filter';
 export { default as insert } from './insert';
+export { default as trace } from './trace';
+
+// Predicates
 export { default as isEmpty } from './isEmpty';
 export { default as isInBounds } from './isInBounds';
 export { default as isInt } from './isInt';
-export { default as reorder } from './reorder';
-export { default as removeAt } from './removeAt';
-export { default as trace } from './trace';
+
+// Result
+export { default as ensure } from './ensure';
 export { default as wrappedErr } from './wrappedErr';
-export * from './compare';
