7f2ffb0a4f040b4b2c5a8d7ee8ea703ca7056e9f
Arthur E. Jones <PartyLich@gmail.com>
2021-03-05 22:50:29 -0600

    feat: implement TreeNode

--- src/util/treeNode.js
+++ src/util/treeNode.js
@@ -22,23 +22,50 @@ const treeNode = <T>({
   parent = null,
   children = [],
 }: options<T> = {}): TreeNode<T> => {
-  const map = (node) => (fn) => {
+  // apply a function to the data at each node in this tree
+  const map = <B>(fn: (T) => B): TreeNode<B> => {
+    if (typeof fn !== 'function') {
+      throw new TypeError('TreeNode: map expected a function as its argument');
+    }
+
+    const mappedData = fn(data);
+    const mappedChildren = children.map((node) => node.map(fn));
+
+    return treeNode({
+      data: mappedData,
+      children: mappedChildren,
+    });
   };
 
-  const toString = () => ``;
+  // fold out a value out by applying a reducer function to each node in this
+  // tree
+  // expects a binary reducer function and an initial accumulator value
+  const reduce = <B>(fn: (B, T) => B, initial: B): B => {
+    if (typeof fn !== 'function') {
+      throw new TypeError('TreeNode: reduce expected a binary function as its argument');
+    }
 
-  const toArray = (node) => () => {
+    return toArray().reduce(fn, initial);
   };
 
-  const reduce = (node) => (fn, initial) => {
+  // returns a string representation of this TreeNode
+  const toString = (): string => `TreeNode ${
+    JSON.stringify(data)
+  } ${
+    JSON.stringify(children.map((a) => a.toString()))
+  }`;
+
+  // returns an array of the data at each node in this tree
+  const toArray = (): Array<T> => {
+    return [data].concat(children.map((a) => a.toArray())).flat();
   };
 
   return {
     data,
     children,
-    map: map(this),
-    reduce: reduce(this),
-    toArray: toArray(this),
+    map,
+    reduce,
+    toArray,
     toString,
     constructor: treeNode,
   };
