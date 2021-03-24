bd935a7f062a1f7a9cd8277ba9bf122cda6abcb6
Arthur E. Jones <PartyLich@gmail.com>
2021-03-24 13:07:48 -0600

    fix: round scaled values to int

--- src/core/scaleAction.ts
+++ src/core/scaleAction.ts
@@ -1,6 +1,14 @@
+import { flow } from '../util';
+
 import { types, Action, Coord } from '../types';
 import { scale } from './';
 
+
+// scale a number and round the result to the nearest integer
+const scaleInt = flow(
+    scale,
+    Math.round,
+);
 
 // scale an action from one resolution (`fromRes`) to another (`toRes`)
 const scaleAction = (fromRes: Coord, toRes: Coord) =>
@@ -8,17 +16,12 @@ const scaleAction = (fromRes: Coord, toRes: Coord) =>
     console.log('scaling action');
     switch (action.type) {
       case types.CLICK:
-        return {
-          ...action,
-          x: scale(fromRes.x, toRes.x, action.x),
-          y: scale(fromRes.y, toRes.y, action.y),
-        };
-
+        // intentional fallthrough
       case types.MDRAG:
         return {
           ...action,
-          x: scale(fromRes.x, toRes.x, action.x),
-          y: scale(fromRes.y, toRes.y, action.y),
+          x: scaleInt(fromRes.x, toRes.x, action.x),
+          y: scaleInt(fromRes.y, toRes.y, action.y),
         };
 
       default:
